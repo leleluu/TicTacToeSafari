4cdf13d4b6c5204f7401ef8d48b4a823de02a9aa
Lele Luu <leleluu@gmail.com>
2023-08-30 09:35:16 +0200

    Remove storyboard

---
+++ TicTacToe/GameViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
